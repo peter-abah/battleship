c87de0877f50403267148875b7120a753ab5476c
Peter Abah <peterabah.ob@gmail.com>
2021-12-09 13:23:10 +0100

    Add tests for receiveAttack method in shipFactory

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -81,3 +81,37 @@ describe('#positions method returns the correct positions of ship', () => {
     expect(ship.positions).toEqual(expectedPositions);
   });
 });
+
+describe('#receiveAttack method returns if attack was sucessful', () => {
+  const ship = shipFactory({
+    startPos: [0, 0],
+    length: 5,
+    orientation: [0, 1],
+  });
+
+  test('Returns true if pos is part of it positions', () => {
+    expect(ship.receiveAttack([0, 4])).toBe(true);
+  });
+
+  test('Returns false if pos is not part of positions', () => {
+    expect(ship.receiveAttack([1, 5])).toBe(false);
+  });
+});
+
+describe('#receiveAttack method adds the pos to attackedPositions if it was sucessful', () => {
+  const ship = shipFactory({
+    startPos: [0, 0],
+    length: 5,
+    orientation: [0, 1],
+  });
+
+  test('Adds pos to attackedPositions when succesful', () => {
+    ship.receiveAttack([0, 3]);
+    expect(ship.attackedPositions).toContain([0, 3]);
+  });
+
+  test('Does not add pos to attackedPositions when not sucessful', () => {
+    ship.receiveAttack([1, 6]);
+    expect(ship.attackedPositions).not.toContain([0, 3]);
+  });
+});
