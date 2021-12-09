5381e74386693319c3d6862c95864c9f4f6f4f1b
Peter Abah <peterabah.ob@gmail.com>
2021-12-09 23:21:35 +0100

    Add tests to check ships do not overlap

--- src/tests/board.test.js
+++ src/tests/board.test.js
@@ -1,4 +1,5 @@
 import boardFactory from '../boardFactory';
+import shipFactory from '../shipFactory';
 
 describe('Creating a new board', () => {
   const board = boardFactory();
@@ -13,5 +14,42 @@ describe('Creating a new board', () => {
 
   test('Board has no ships', () => {
     expect(board.ships.length).toBe(0);
+  });
+});
+
+describe('Creating a new board with ships', () => {
+  test('ships property contains ships passed to board', () => {
+    const ship = shipFactory({
+      startPos: [0, 0],
+      length: 2,
+      orientation: [0, 1],
+    });
+
+    const board = boardFactory({ ships: [ship] });
+
+    expect(board.ships).toContain(ship);
+  });
+
+  test('ships contains no ships if it passed any ship', () => {
+    const board = boardFactory();
+    expect(board.ships.length).toBe(0);
+  });
+});
+
+describe('Validates ship positions are not overlapping', () => {
+  test('throws error if ships overlap', () => {
+    const ship1 = shipFactory({
+      startPos: [0, 0],
+      length: 4,
+      orientation: [0, 1],
+    });
+
+    const ship2 = shipFactory({
+      startPos: [0, 4],
+      length: 3,
+      orientation: [0, -1],
+    });
+
+    expect(() => boardFactory({ ships: [ship1, ship2] })).toThrowError();
   });
 });
