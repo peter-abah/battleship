c52d1ecc2d889f5cd6e5d75ef72f11fa9318d788
Peter Abah <peterabah.ob@gmail.com>
2021-12-12 19:39:41 +0100

    Remove genAllBoardPositions from computerFactory

--- src/computerPlayerFactory.js
+++ src/computerPlayerFactory.js
@@ -4,23 +4,13 @@ import { randomElement } from './helperFuncs';
 import { randomBattleShipBoard } from './boardFactory';
 
 const computerPlayerFactory = () => {
-  const genAllBoardPositions = (board) => {
-    const result = [];
-    for (let y = 0; y < board.height; y += 1) {
-      for (let x = 0; x < board.width; x += 1) {
-        result.push([y, x]);
-      }
-    }
-    return result;
-  };
-
   const board = randomBattleShipBoard();
   const self = { board };
 
   const makeMove = (_, { player, board: opponentBoard }) => {
     if (player !== self) return;
 
-    const positions = genAllBoardPositions(opponentBoard);
+    const positions = opponentBoard.allIndices;
     const pos = randomElement(positions);
     PubSub.publish(eventTypes.PLAYER_MOVE, { player: self, pos });
   };
