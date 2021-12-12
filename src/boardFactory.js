6fca0f4bd30d4757e9df3c88a4be9f53ea375637
Peter Abah <peterabah.ob@gmail.com>
2021-12-12 15:51:15 +0100

    Refactor randomBattleShipBoard

--- src/boardFactory.js
+++ src/boardFactory.js
@@ -1,3 +1,6 @@
+import { randomElement } from './helperFuncs';
+import shipFactory from './shipFactory';
+
 const DEFAULT_WIDTH = 10;
 const DEFAULT_HEIGHT = 10;
 
@@ -26,8 +29,8 @@ const boardFactory = ({
   const validateShips = () => {
     const shipsPositions = getAllShipsPositions(ships);
     if (
-      isShipsOutOfBounds(shipsPositions)
-      || isShipsOverlapping(shipsPositions)
+      isShipsOutOfBounds(shipsPositions) ||
+      isShipsOverlapping(shipsPositions)
     ) {
       throw new Error('Invalid ship placements');
     }
@@ -58,8 +61,8 @@ const boardFactory = ({
   const addShip = (ship) => {
     const updatedShipPositions = getAllShipsPositions([...ships, ship]);
     if (
-      isShipsOverlapping(updatedShipPositions)
-      || isShipsOutOfBounds(updatedShipPositions)
+      isShipsOverlapping(updatedShipPositions) ||
+      isShipsOutOfBounds(updatedShipPositions)
     ) {
       return false;
     }
@@ -87,4 +90,60 @@ const boardFactory = ({
   return self;
 };
 
+const randomBattleShipBoard = () => {
+  const orientations = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  const shipLengths = [5, 4, 3, 3, 2];
+
+  const genAllBoardPositions = (board) => {
+    const result = [];
+    for (let y = 0; y < board.height; y += 1) {
+      for (let x = 0; x < board.width; x += 1) {
+        result.push([y, x]);
+      }
+    }
+    return result;
+  };
+
+  const addShipToBoard = (length) => {
+    const randomPos = randomElement(boardPositions);
+
+    const isShipCreated = orientations.some((orientation) => {
+      const ship = shipFactory({
+        startPos: randomPos,
+        length,
+        orientation,
+      });
+      const isShipAdded = board.addShip(ship);
+      return isShipAdded;
+    });
+
+    const positions = boardPositions.filter((elem) => elem !== randomPos);
+
+    return { positions, isShipCreated };
+  };
+
+  const addShips = () => {
+    shipLengths.forEach((length) => {
+      let isShipCreated = false;
+
+      while (!isShipCreated) {
+        ({ isShipCreated, positions: boardPositions } = addShipToBoard(length));
+      }
+    });
+  };
+
+  const board = boardFactory();
+  let boardPositions = genAllBoardPositions(board);
+  addShips();
+
+  return board;
+};
+
 export default boardFactory;
+export { boardFactory, randomBattleShipBoard };
