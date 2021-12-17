4cd0629e9f5b286264b0d34ca6bcf61d43fe4200
Peter Abah <peterabah.ob@gmail.com>
2021-12-17 19:03:09 +0100

    Add more events to eventTypes

--- src/eventTypes.js
+++ src/eventTypes.js
@@ -3,6 +3,9 @@ const eventTypes = {
   UPDATE_UI: 'update ui',
   MOVE_INPUT: 'move input',
   PLAYER_MOVE: 'player move',
+  UPDATE_BOARD: 'update board',
+  GAME_START: 'game start',
+  GAME_END: 'game end',
 };
 
 Object.freeze(eventTypes);
