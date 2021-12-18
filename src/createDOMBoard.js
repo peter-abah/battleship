ef7b14776b14545a39b147b96f14e08515c961e0
Peter Abah <peterabah.ob@gmail.com>
2021-12-18 12:24:05 +0100

    Refactor createDOMBoard module

--- src/createDOMBoard.js
+++ src/createDOMBoard.js
@@ -2,7 +2,12 @@ import PubSub from 'pubsub-js';
 import eventTypes from './eventTypes';
 
 const isShipPos = (board, pos) => board.ships.some((ship) => ship.isPos(pos));
-const isSunkShipPos = (board, pos) => board.sunkShips().some((ship) => ship.isPos(pos));
+
+const isSunkShipPos = (board, pos) =>
+  board.sunkShips().some((ship) => ship.isPos(pos));
+
+const isAttackedShipPos = (board, [y, x]) =>
+  board.attackedShipsPositions.some((pos) => pos[0] === y && pos[1] === x);
 
 const addClassNamesToCell = (cell, board, pos) => {
   cell.classList.add('board__cell');
@@ -15,18 +20,26 @@ const addClassNamesToCell = (cell, board, pos) => {
     cell.classList.add('board__cell--ship');
   }
 
+  if (isAttackedShipPos(board, pos)) {
+    cell.classList.add('board__cell--attacked-ship');
+  }
+
   if (isSunkShipPos(board, pos)) {
     cell.classList.add('board__cell--sunk-ship');
   }
 };
 
 const createDOMBoard = (board) => {
+  const getCell = ([y, x]) => {
+    const cellSelector = `[data-pos="${y}${x}"]`;
+    return boardDom.querySelector(cellSelector);
+  };
+  
   const updateBoard = (_, data) => {
     if (data !== boardDom) return;
 
-    board.attackedPositions.forEach(([y, x]) => {
-      const cellSelector = `[data-pos="${y}${x}"]`;
-      const cell = boardDom.querySelector(cellSelector);
+    board.attackedPositions.forEach((pos) => {
+      const cell = getCell(pos);
       cell.classList.add('board__cell--attacked');
     });
 
@@ -34,20 +47,23 @@ const createDOMBoard = (board) => {
       (acc, ship) => acc.concat(ship.positions),
       []
     );
-    shipsPos.forEach(([y, x]) => {
-      const cellSelector = `[data-pos="${y}${x}"]`;
-      const cell = boardDom.querySelector(cellSelector);
+    shipsPos.forEach((pos) => {
+      const cell = getCell(pos);
       cell.classList.add('board__cell--ship');
     });
 
-    const sunkShipsPos = board.sunkShips().reduce(
-      (acc, ship) => acc.concat(ship.positions),
-      []
-    );
-    sunkShipsPos.forEach(([y, x]) => {
-      const cellSelector = `[data-pos="${y}${x}"]`;
-      const cell = boardDom.querySelector(cellSelector);
+    const sunkShipsPos = board
+      .sunkShips()
+      .reduce((acc, ship) => acc.concat(ship.positions), []);
+
+    sunkShipsPos.forEach((pos) => {
+      const cell = getCell(pos);
       cell.classList.add('board__cell--sunk-ship');
+    });
+
+    board.attackedShipsPositions.forEach((pos) => {
+      const cell = getCell(pos);
+      cell.classList.add('board__cell--attacked-ship');
     });
   };
 
